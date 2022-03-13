ca0948131d530ca9eef5cc11088203d306a5c2db
AristideDembele <dizo.dembele4@outlook.fr>
2022-03-13 11:07:09 -0700

    Task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int number;
+
+  for (number =31; number <= 45; number++)
+    {putchar(number);
+      if (number == 45);
+      {break;
+      }
+      putchar(',');
+      putchar(' ');
+    }
+  putchar('\n');
+  return (0);
+}
